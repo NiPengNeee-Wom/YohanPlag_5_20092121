d7f617be5e63b6d62715a774aac14620df1986d7
NiPengNeee-Wom <zaphod1er@gmail.com>
2021-10-11 11:27:38 +0200

    Fin optimisation JS page Product

--- front/js/product.js
+++ front/js/product.js
@@ -1,5 +1,4 @@
-    // Recupération données localStorage et calcul total price
-function retrieveAndDisplayStorageCount(colorChoice, product){
+function retrieveAndDisplayStorageCount(colorChoice, product){                  // Fonction de Recupération données localStorage et calcul total price
     let archive = [];
     let returnArray = [true, 0];
     for (var i = 0; i<localStorage.length; i++) {
@@ -24,7 +23,7 @@ class Canap{
     }
 }
 
-async function fetchApi(idProduct){
+async function fetchApi(idProduct){                                             // Fonction de Contact Api
     return fetch(`http://localhost:3000/api/products/` + idProduct)
     .then(function(res){
         if(res.ok){
@@ -36,26 +35,17 @@ async function fetchApi(idProduct){
     })
 }
 
-    // Display du produit
-const displayProducts = (product) => {                              
-
-    // Gestion Image
-    const parentNodeImg = document.querySelector("div.item__img");  
+const displayProducts = (product) => {                                          // Fonction de Display du produit         
+    const parentNodeImg = document.querySelector("div.item__img");              // Display Image              
     const newImg = document.createElement("img");                   
     newImg.setAttribute("src", product.imageUrl);
     newImg.setAttribute("alt", product.altTxt);
     parentNodeImg.appendChild(newImg);
-
-    // Gestion Nom produit
-    const nameNode = document.getElementById("title");
+    const nameNode = document.getElementById("title");                          // Display Nom produit
     nameNode.innerText = product.name;
-
-    // Gestion Prix Produit
-    const priceNode = document.getElementById("price");
+    const priceNode = document.getElementById("price");                         // Display Prix Produit
     priceNode.innerText = product.price;
-
-    // Gestion Couleur
-    const colorNode = document.getElementById("colors");
+    const colorNode = document.getElementById("colors");                        // Display Couleur
     let colorTab = product.colors;
     let colorCount = 0;
     for(element of colorTab){
@@ -65,18 +55,12 @@ const displayProducts = (product) => {
         colorNode.appendChild(newOption);
         colorCount++;
     }
-
-    // Gestion Description
-    const descriptionNode = document.getElementById("description");
+    const descriptionNode = document.getElementById("description");             // Display Description
     descriptionNode.innerText = product.description;
-
 }
 
-    // Saving data
-const savingDataLocalStorage = (product) =>{                 
-
-    // Stockage couleur
-    let colorChoice = "";
+const savingDataLocalStorage = (product) =>{                                    // Fonction Sauvegarde LocalStorage         
+    let colorChoice = "";                                                       // Stockage couleur
     let colorChoiceNode = document.getElementById("colors");
     const quantityParentNode = document.querySelector("div.item__content__settings");
     const quantityInfo = document.createElement("p");
@@ -92,27 +76,19 @@ const savingDataLocalStorage = (product) =>{
             }
         }                                                                   
     });
-
-    // Stockage quantity
-    let productCount = 0;
+    let productCount = 0;                                                       // Stockage quantity
     let productCountNode = document.getElementById("quantity");
     productCountNode.addEventListener('input', function(eventInp){
     productCount = eventInp.target.value;
     });
-
-    // Creation instance pour push Storage
-
-    const erreurInfoNode = document.querySelector("div.item__content");
+    const erreurInfoNode = document.querySelector("div.item__content");         // Creation Message d'erreur     
     const erreurInfo = document.createElement("p");
     erreurInfo.setAttribute("style", "color:#bb0b0b; font-weight:bold;");
     erreurInfo.innerText = "    Veuillez choisir une couleur et une quantité!";
-
-    let eventButton = document.getElementById("addToCart");
+    let eventButton = document.getElementById("addToCart");                     // Creation instance pour push Storage
     eventButton.addEventListener('click', function(){
         erreurInfo.remove();
         let dataProduct = new Canap(product.altTxt, colorChoice ,product.description, product.imageUrl, product.name, product.price, product._id, productCount);
-        
-        // LocalStorageTab.push(dataProduct); injection class dans storage
         if (productCount >= 1 && colorChoice != "" && productCount <= 100){
         let stringDataProduct = JSON.stringify(dataProduct);
         localStorage.setItem(product.name + " " + colorChoice, stringDataProduct);
@@ -123,12 +99,11 @@ const savingDataLocalStorage = (product) =>{
     })
 }
 
-
 async function main(){
-    var idProduct = document.location.href.split("id=");    // Spit de l'id dans l'url
-    let product = await fetchApi(idProduct[1]);             // Fetch avec id
-    displayProducts(product);                               // Display du produit
-    savingDataLocalStorage(product);                        // Stockage info local storage
+    var idProduct = document.location.href.split("id=");                        // Spit de l'id dans l'url
+    let product = await fetchApi(idProduct[1]);                                 // Fetch avec id
+    displayProducts(product);                                                   // Display du produit
+    savingDataLocalStorage(product);                                            // Stockage info local storage
 }
 
 main();
